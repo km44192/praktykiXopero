31ff6f8ed4cb7cfdcc60a7bc9224887bdad2c769
km44192 <48087059+km44192@users.noreply.github.com>
2020-10-20 12:13:52 +0200

    Pierwszy_commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Praktyki_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
