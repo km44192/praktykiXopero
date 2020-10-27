54bc6c0250bfa668e85fd44fe2ac93f7c9c5de6d
Marcin Krawczyk <km44192@zut.edu.pl>
2020-10-27 10:02:44 +0100

    Start 27-10-2020 10:00

---
+++ Obcy_Atakuja/Obcy_atakuja/Obcy_atakuja/My Project/AssemblyInfo.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' skojarzone z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("Obcy_atakuja")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Obcy_atakuja")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza 
+'      Numer kompilacji
+'      Rewizja
+'
+' Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki 
+' przy użyciu symbolu „*”, tak jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: ComVisible(False)>
