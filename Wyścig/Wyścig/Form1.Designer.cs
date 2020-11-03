56be6e7153265d5ccf236242f99b77656ae0c8ad
Marcin Krawczyk <km44192@zut.edu.pl>
2020-11-03 10:07:10 +0100

    Start 03-11-2020 10:00

--- Wyścig/Wyścig/Form1.Designer.cs
+++ Wyścig/Wyścig/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -71,6 +74,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -80,6 +84,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -89,12 +94,28 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(314, 236);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(443, 285);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -116,6 +137,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Wyścig/Wyścig/Form1.cs
+++ Wyścig/Wyścig/Form1.cs
@@ -18,13 +18,43 @@ namespace Wyścig
         }
         Wlepy[] biegacz = new Wlepy[3];
 
-        private void move(int index,Label info)
+        private void move1(int index,Label info)
         {
-            if (biegacz[index] == null) { biegacz[index] == new Wlepy();
-           biegacz}
-
+            if (biegacz[index] == null) { biegacz[index] = new Wlepy();
+                biegacz[index].MyLabel = info;
+            }
+            else { biegacz[index] = null; }
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            move1(0, label1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            move1(1, label2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            move1(2, label3);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if (biegacz[i] != null) { biegacz[i].move(); }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true ;
+        }
     }
 }
